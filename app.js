e1073c7c17d73d7fa3c43c63331e7dfa44c0195b
nwabunwene <nwabuosa@gmail.com>
2025-05-01 10:23:00 +0100

    Testing out Javascript: Variable assignment, template literals, etc

---
+++ app.js
@@ -0,0 +1,33 @@
+let addition = 5 + 6;
+console.log(addition);
+
+console.log("Welcome to my website");
+
+let favFood = "white soup";
+console.log(`My favorite food is ${favFood}`);
+
+let myName = "Wene";
+console.log(myName);
+
+let age = 30;
+console.log(age);
+
+console.log(`My name is ${myName} and I am ${age} years old`);
+
+let five = 5;
+let six = 6;
+let negative = -1;
+let bool = six > five;
+let bool0 = six < five;
+
+console.log(`Is ${six} > ${five} ?`, bool);
+console.log(`${six}<${five}`, bool0);
+
+console.log(bool);
+
+let favProgLang = "python";
+let newLearn = "JS, HTML & Python";
+
+console.log(
+  `My favorite programming language is ${favProgLang} but I'm now learning ${newLearn} and finding it fun!`
+);
